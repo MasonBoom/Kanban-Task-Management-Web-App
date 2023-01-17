ad19b9bb0c9a2bb325bed5049a747ffc7fe3ab23
MasonBoom <masonboom@gmail.com>
2023-01-17 07:21:37 -0500

    file structure setup

--- src/mainPage/MainPage.js
+++ src/mainPage/MainPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+function MainPage() {
   return (
-    <div className="App">
+    <div>
       <h1>Hello World</h1>
     </div>
   );
 }
 
-export default App;
+export default MainPage;
