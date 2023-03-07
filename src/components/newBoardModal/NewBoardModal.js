5e527c179f3c42018edad9278957cc9216b51941
MasonBoom <masonboom@gmail.com>
2023-03-06 19:12:31 -0500

    reverted non working changes

--- src/components/newBoardModal/NewBoardModal.js
+++ src/components/newBoardModal/NewBoardModal.js
@@ -15,37 +15,16 @@ import x from '../../icons/x.svg';
 import grayX from '../../icons/grayX.svg';
 
 function NewBoardModal({ isVisible = false, onCancel }) {
-
-  const Column = ({ index, value }) => {
-    const [inputValue, setInputValue] = useState('');
-
-    const removeItem = (index) => {
-      if(columns.length > 1) {
-        columns.splice(index, 1);
-        setColumns([...columns]);
-      }
-    }
-
-    const handleChange = (e) => {
-      setInputValue(e.target.value);
-      value = inputValue
-    }
-
-    return (
-      <ColumnItem>
-        <ColumnsInput 
-          type="text" 
-          placeholder="e.g. To Do, In Progress, Done" 
-          onChange={handleChange} 
-          value={inputValue}
-        />
-        <GrayX src={grayX} alt="grayX" onClick={removeItem} />
-      </ColumnItem>
-    )
-  }
-
   const [modalVisible, setModalVisible] = useState(isVisible);
-  const [columns, setColumns] = useState([<Column />]);
+  const [columns, setColumns] = useState([
+    <ColumnItem>
+      <ColumnsInput 
+        type="text" 
+        placeholder="e.g. To Do, In Progress, Done" 
+      />
+      <GrayX src={grayX} alt="grayX" />
+    </ColumnItem>
+  ]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -58,7 +37,15 @@ function NewBoardModal({ isVisible = false, onCancel }) {
 
   const handleAddColumn = () => {
     if(columns.length < 5) {
-      setColumns([...columns, <Column />]);
+      setColumns([...columns,
+        <ColumnItem>
+          <ColumnsInput 
+            type="text" 
+            placeholder="e.g. To Do, In Progress, Done" 
+          />
+          <GrayX src={grayX} alt="grayX" />
+        </ColumnItem> 
+      ]);
       console.log(columns)
     } else {
       alert('You can only have 5 columns per board');
@@ -74,13 +61,7 @@ function NewBoardModal({ isVisible = false, onCancel }) {
           <label htmlFor="board-name">Name</label>
           <NameInput type="text" placeholder="e.g. Web Design" />
           <label htmlFor="columns">Columns</label>
-          {
-            columns.map((column, i) => {
-              return (
-                <Column index={i} />
-              )
-            })
-          }
+          {columns}
           <ButtonDiv>
             <AddColumn onClick={handleAddColumn}>+ Add New Column</AddColumn>
             <CreateBoard type="submit">Create Board</CreateBoard>
